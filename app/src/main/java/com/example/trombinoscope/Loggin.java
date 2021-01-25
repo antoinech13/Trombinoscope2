688a9de221f61e30c8205b03c8c20c3677d146ed
Marilyn Begnis <marilyn.begnis@etu.univ-amu.fr>
2021-01-25 19:22:21 +0100

    RememberMe Loggin

--- app/src/main/java/com/example/trombinoscope/Loggin.java
+++ app/src/main/java/com/example/trombinoscope/Loggin.java
@@ -2,11 +2,17 @@ package com.example.trombinoscope;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.android.volley.Request;
@@ -27,6 +33,8 @@ public class Loggin extends AppCompatActivity {
     private Button btn_loggin;
     private EditText pseudo, password;
     private JSONObject js = new JSONObject();
+    private CheckBox rememberCheckbox;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +44,15 @@ public class Loggin extends AppCompatActivity {
         btn_loggin = findViewById(R.id.login);
         pseudo = findViewById(R.id.username);
         password = findViewById(R.id.password);
+        rememberCheckbox = findViewById(R.id.rememberMe);
+        SharedPreferences userValuesSave = getSharedPreferences("userValuesSave", MODE_PRIVATE);
+        SharedPreferences.Editor editor = userValuesSave.edit();
+
+        // Recupération des données users si la checkbox a été cochée
+        pseudo.setText(userValuesSave.getString("userName", ""));
+        password.setText(userValuesSave.getString("userPw", ""));
+        rememberCheckbox.setChecked(userValuesSave.getBoolean("CheckBox", false));
+
 
         btn_loggin.setOnClickListener(new View.OnClickListener() {
 
@@ -44,22 +61,32 @@ public class Loggin extends AppCompatActivity {
                 String Pseudo = pseudo.getText().toString().trim();
                 String Password = password.getText().toString().trim();
 
+                // Saved values if checked checkbox
+                if (rememberCheckbox.isChecked()) {
+                    editor.putString("userName", Pseudo);
+                    editor.putString("userPw", Password);
+                    editor.putBoolean("CheckBox", true);
+
+                    editor.commit();
+                    Snackbar.make(btn_loggin, "Saved", 2000).show();
+                }
+                else {
+                    editor.clear();
+                    editor.commit();
+                }
+                Log.d("yolo", "2");
                 if (!Pseudo.isEmpty() || !Password.isEmpty()) {
                     Login(Pseudo, Password);
-
                 } else {
                     Snackbar.make(v, "pls Enter valide values", 2000).show();
                 }
-
-
             }
         });
-
-
     }
 
-    private void Login(String pseudo, String password) {
-        String url = "https://192.168.1.21:5000/";
+
+    private void Login (String pseudo, String password){
+        String url = "https://192.168.1.13:5000/";
         update(pseudo, password);
 
         RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
@@ -88,8 +115,8 @@ public class Loggin extends AppCompatActivity {
         queue.add(jsonObjectRequest);
     }
 
-    private void update(String username, String password) {
-        try{
+    private void update (String username, String password){
+        try {
             js.put("request", "authentification");
             js.put("username", username);
             js.put("password", password);
@@ -97,8 +124,4 @@ public class Loggin extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-
-
-
-
 }
